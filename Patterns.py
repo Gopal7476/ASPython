bd84f26f79420763a4c5827f2273d1941afd880e
Gopal7476 <82716272+Gopal7476@users.noreply.github.com>
2021-08-28 16:47:25 +0530

    Add files via upload

---
+++ Patterns.py
@@ -0,0 +1,146 @@
+"""
+printing all the columns and rows
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    for j in range(1,a+1):
+##        print("*",end=" ")
+##    print()   
+
+
+"""
+printing the right angle triangle pattern
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    for j in range(1,i+1):
+##        print("*",end=" ")
+##    print()    
+
+
+"""
+printing the right angle triangle pattern mirror
+"""
+##a=int(input())
+##b=a
+##for i in range(1,a+1):
+##    for j in range(1,b+1):
+##        print("*",end=" ")
+##    b-=1
+##    print()
+
+
+"""
+printing the right angle triangle pattern opposite
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    h=a-i
+##    for j in range(0,h):
+##        print(" ",end=" ")
+##    for k in range(1,i+1):
+##        print("*",end=" ")
+##    print()    
+
+
+
+"""
+printing the right angle triangle pattern opposite mirror
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    h=i-1
+##    for j in range(1,h+1):
+##        print(" ",end=" ")
+##    g=a-h    
+##    for k in range(1,g+1):
+##        print("*",end=" ")
+##    print()
+
+
+"""
+printing alphabets
+"""
+##for i in range(65,70):
+##    for j in range(65,i+1):
+##        print(chr(j),end="")
+##    print()    
+
+
+
+"""
+printing alphabets
+"""
+##for i in range(65,70):
+##    for j in range(65,i+1):
+##        print(chr(i),end="")
+##    print()
+
+
+"""
+printing numbering
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    for j in range(1,i+1):
+##        print(i,end=" ")
+##    print()    
+
+
+
+"""
+printing numbering in right side
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    for j in range(1,i+1):
+##        print(j,end=" ")
+##    print()
+
+
+
+"""
+printing triangle pattern
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    h=a-i
+##    for j in range(1,h+1):
+##        print("",end=" ")
+##    for k in range(1,i+1):
+##        print("*",end=" ")
+##    print()
+
+
+"""
+printing triangle in revere pattern
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    for j in range(0,i-1):
+##        print("",end=" ")
+##    h=a-i+1    
+##    for k in range(1,h+1):
+##        print("*",end=" ")
+##    print()    
+
+
+"""
+printing diamond pattern
+"""
+##a=int(input())
+##for i in range(1,a+1):
+##    for j in range(1,a-i+1):
+##        print("",end=" ")
+##    for k in range(1,i+1):
+##        print("*",end=" ")
+##    print()
+##for i in range(1,a+1):    
+##    for j in range(0,i):
+##        print("",end=" ")
+##    for k in range(0,a-i):
+##        print("*",end=" ")
+##    print()    
+    
+
+
